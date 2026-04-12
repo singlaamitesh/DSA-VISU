5beeaf43f6580b9035b6f644680410edcf3bbfe1
singlaamitesh <singlaamitesh@gmail.com>
2026-04-12 11:48:35 +0530

    feat: register all 12 algorithms in registry

--- src/algorithms/registry.ts
+++ src/algorithms/registry.ts
@@ -1,6 +1,31 @@
 import { AlgorithmConfig } from './types';
+import { bubbleSort } from './sorting/bubbleSort';
+import { selectionSort } from './sorting/selectionSort';
+import { insertionSort } from './sorting/insertionSort';
+import { mergeSort } from './sorting/mergeSort';
+import { quickSort } from './sorting/quickSort';
+import { linearSearch } from './searching/linearSearch';
+import { binarySearch } from './searching/binarySearch';
+import { bfs } from './graph/bfs';
+import { dfs } from './graph/dfs';
+import { dijkstra } from './graph/dijkstra';
+import { fibonacci } from './dp/fibonacci';
+import { knapsack } from './dp/knapsack';
 
-const algorithms: AlgorithmConfig[] = [];
+const algorithms: AlgorithmConfig[] = [
+  bubbleSort,
+  selectionSort,
+  insertionSort,
+  mergeSort,
+  quickSort,
+  linearSearch,
+  binarySearch,
+  bfs,
+  dfs,
+  dijkstra,
+  fibonacci,
+  knapsack,
+];
 
 export const getAlgorithms = () => algorithms;
 
